d27b0e00aa231b44f05f51155e1e4c67fd4b4a3e
bruce lawson <bruce@brucelawson.co.uk>
2021-04-28 17:39:14 +0100

    proper scope

--- dist/tota11y.js
+++ dist/tota11y.js
@@ -9,7 +9,7 @@
  * Released under the MIT license
  * http://github.com/Khan/tota11y/blob/master/LICENSE.txt
  * 
- * Date: 2021-04-23
+ * Date: 2021-04-28
  * 
  */
 /******/ (function(modules) { // webpackBootstrap
